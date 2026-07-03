7cfc6fe7c9866cb5816c6d565ba190be7585ec4c
Freelancer <freelancer@REACH-IMAC03.local>
2026-07-03 14:38:25 +0100

    [2026-07-03 main] Made page look nice

--- resources/views/layouts/app/header.blade.php
+++ resources/views/layouts/app/header.blade.php
@@ -2,6 +2,21 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
     <head>
         @include('partials.head')
+        <style>
+        body {
+            background-color: lightblue;
+        }
+
+        h1 {
+            color: darkgreen;
+            text-align: center;
+        }
+
+        p {
+            font-family: verdana;
+            font-size: 20px;
+        }
+        </style>
     </head>
     <body class="min-h-screen bg-white dark:bg-zinc-800">
         <flux:header container class="border-b border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
